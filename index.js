361f1c725939c5e8998129ca2f9716d5673cb15c
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-12 22:35:22 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+let sampleObjects = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(arrayOfObjects) {
+  let foundItem = arrayOfObjects.find((item) => item.result === "W");
+  return foundItem === undefined ? undefined : foundItem["year"];
+}
